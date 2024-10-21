e6320d67bee61c10d3f92a66427ca48179530f0a
Hazret-R <302173137@student.rocmondriaan.nl>
2024-10-21 15:08:54 +0200

    Variabelen

---
+++ variabelen/Startpunt/scripts/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('mode-toggle').addEventListener('click', function() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    } else {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }
+});
